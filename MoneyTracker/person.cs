b50f7d3c9103556208853f33b63207f6f33e367e
Ortak <Ortak@LAPTOP-FBK0BP45>
2021-01-09 20:21:25 +0100

    c || NEw

--- MoneyTracker/person.cs
+++ MoneyTracker/person.cs
@@ -8,10 +8,10 @@
 
             public int Alter { get; set; }
 
-            public Person(string fn, string ln)
+            public Person(string first, string last)
             {
-                First = fn;
-                Last = ln;
+                First = first;
+                Last = last;
             }
 
             public string First { get; }
