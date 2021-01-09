d9b812bd53195024e14f1cb69e90925e1edec7ee
Ortak <Ortak@LAPTOP-FBK0BP45>
2021-01-09 19:18:54 +0100

    add new prop

--- MoneyTracker/person.cs
+++ MoneyTracker/person.cs
@@ -6,8 +6,11 @@ namespace MoneyTracker
 {
     class person
     {
+        
         class Person
         {
+            public int Alter { get; set; }
+
             public Person(string fn, string ln)
             {
                 First = fn;
